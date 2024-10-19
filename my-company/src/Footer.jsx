ab81a1b15db6d055696080e4acc9c041bfaf61ef
Oussama Weqqasse <oussama.wqs@gmail.com>
2024-10-19 20:16:25 +0100

    Home.jsx

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,11 @@
+// src/Footer.jsx
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', textAlign: 'center', backgroundColor: '#ddd' }}>
+        <p>&copy; 2024 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
