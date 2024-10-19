520937e0b0813e22060baa6fba982f4c8a97b150
Oussama Weqqasse <oussama.wqs@gmail.com>
2024-10-19 17:50:49 +0100

    Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }}>
+            <p style={{ margin: '0' }}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
+
