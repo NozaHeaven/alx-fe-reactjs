62dc152ce53e941766bd1abd52ca345fa705a641
Oussama Weqqasse <oussama.wqs@gmail.com>
2024-10-19 16:10:21 +0100

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,6 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -28,6 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+            <WelcomeMessage />
+            {/* Other components or content can go here */}
+        </div>
     </>
   )
 }
