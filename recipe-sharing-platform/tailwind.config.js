5bce9855dab767608f27b9cf444d985ed92bd725
Oussama Weqqasse <oussama.wqs@gmail.com>
2024-10-20 14:16:53 +0100

    App.jsx

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Optionally set to 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
