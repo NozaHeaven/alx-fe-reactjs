29baa3b8547320f8f78798568087250739675c1e
Oussama Weqqasse <oussama.wqs@gmail.com>
2024-10-20 05:03:42 +0100

    index.css

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -24,6 +26,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <UserProfile />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
